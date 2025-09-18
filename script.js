0e2b54ead0bbd1faf8bd8331448f8a1c57ec6b46
Genius Kudzai <168020148+geniuskudzai21@users.noreply.github.com>
2025-09-18 10:26:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -613,10 +613,10 @@ function createWordCloud(wordFrequency) {
     const container = document.getElementById('wordCloud');
     container.innerHTML = '';
     
-    // Get top 10 words only
+    // Get top 20 words only
     const words = Object.entries(wordFrequency)
         .sort((a, b) => b[1] - a[1])
-        .slice(0,21);
+        .slice(0, 20);
     
     if (words.length === 0) {
         container.innerHTML = '<p class="text-gray-500">Not enough data to generate word cloud</p>';
@@ -764,3 +764,4 @@ function disableDarkMode() {
     localStorage.setItem('darkMode', 'false');
 
 }
+
